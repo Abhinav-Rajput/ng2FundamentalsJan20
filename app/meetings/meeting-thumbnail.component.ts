ca615f113da1f2dc911f71e0f72d5925ab301c89
Abhinav Kumar Singh <abhinav.singh@tavant.com>
2017-02-13 12:58:16 +0530

    [hidden] attribute]

--- app/meetings/meeting-thumbnail.component.ts
+++ app/meetings/meeting-thumbnail.component.ts
@@ -8,12 +8,12 @@ import {Component, Input, Output, EventEmitter } from '@angular/core'
     <div>Date:  {{events?.date}}</div>
     <div>Time: {{events?.time}}</div>    
     <div>Price: \${{events?.price}}</div>
-    <div *ngIf="events?.location">
+    <div [hidden]="!events?.location">
         <span>Location: {{events?.location?.address}}</span>
         <span class="pad-left"></span>
         <span>{{events?.location?.city}}, {{events?.location?.country}}</span>
     </div>
-<div *ngIf="events?.onlineUrl">Online URL: {{events?.onlineUrl}}
+<div [hidden]="!events?.onlineUrl">Online URL: {{events?.onlineUrl}}
 </div>
   
     </div>`
