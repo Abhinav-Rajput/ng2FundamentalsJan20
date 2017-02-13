2e86df8cf836188395130f63bbbea4523d942314
Abhinav Kumar Singh <abhinav.singh@tavant.com>
2017-02-13 16:35:36 +0530

    re-usable service & toastr

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,12 @@ import { EventsAppComponent }  from './events-app.component'
 import {MeetingsListComponent }  from './meetings/meetings-list.component'
 import {MeetingThumbnailComponet} from './meetings/meeting-thumbnail.component'
 import {NavBarComponent} from './nav/navbar.component'
+import {MeetingService} from './meetings/shared/meeting-service'
+import {ToastrService} from './common/toastr.service'
 @NgModule({
     imports:[BrowserModule],
     declarations:[EventsAppComponent,MeetingsListComponent,MeetingThumbnailComponet,NavBarComponent],
+    providers: [MeetingService,ToastrService],
     bootstrap: [EventsAppComponent]
 })
 
